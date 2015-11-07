29d94c821273c1468675cfe43671e46575524353
Andrew Bailey <marverenic@gmail.com>
2015-11-07 13:44:56 -0500

    New album page & improved artist page  - Added album artwork to AlbumActivity  - Made status bar transparent when artwork is visible (API 21+)  - Added scrim to artist images  - Fixed spelling of "scrim"

--- app/src/main/java/com/marverenic/music/activity/instance/AlbumActivity.java
+++ app/src/main/java/com/marverenic/music/activity/instance/AlbumActivity.java
@@ -1,15 +1,20 @@
 package com.marverenic.music.activity.instance;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.marverenic.music.Library;
 import com.marverenic.music.R;
 import com.marverenic.music.activity.BaseActivity;
 import com.marverenic.music.instances.Album;
-import com.marverenic.music.Library;
 import com.marverenic.music.instances.Song;
 import com.marverenic.music.instances.viewholder.SongViewHolder;
 import com.marverenic.music.utils.Themes;
@@ -27,11 +32,20 @@ public class AlbumActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_instance);
+        setContentView(R.layout.activity_instance_artwork);
 
         reference = getIntent().getParcelableExtra(ALBUM_EXTRA);
         data = Library.getAlbumEntries(reference);
         if (getSupportActionBar() != null) getSupportActionBar().setTitle(reference.albumName);
+
+        Glide.with(this).load(reference.artUri)
+                .centerCrop()
+                .into((ImageView) findViewById(R.id.backdrop));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
         RecyclerView songRecyclerView = (RecyclerView) findViewById(R.id.list);
         songRecyclerView.setAdapter(new Adapter());
--- app/src/main/java/com/marverenic/music/activity/instance/ArtistActivity.java
+++ app/src/main/java/com/marverenic/music/activity/instance/ArtistActivity.java
@@ -4,10 +4,12 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
@@ -95,6 +97,11 @@ public class ArtistActivity extends BaseActivity {
 
         CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle(reference.artistName);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
         final Adapter adapter = new Adapter();
