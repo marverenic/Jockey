19d531991e89b36197be063b5829bc783ee651da
Andrew Bailey <marverenic@gmail.com>
2015-11-06 23:21:57 -0500

    Updated build config  - Simplified Crashlytics instantiation  - Incremented build version  - Removed "-gp" suffix for Google Play version codes

--- app/src/main/java/com/marverenic/music/JockeyApplication.java
+++ app/src/main/java/com/marverenic/music/JockeyApplication.java
@@ -1,10 +1,7 @@
 package com.marverenic.music;
 
 import android.app.Application;
-
 import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.core.CrashlyticsCore;
-
 import io.fabric.sdk.android.Fabric;
 
 public class JockeyApplication extends Application {
@@ -12,10 +9,8 @@ public class JockeyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this,
-                new Crashlytics.Builder()
-                        .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
-                        .build());
+        Fabric.with(this, new Crashlytics());
+
         PlayerController.startService(getApplicationContext());
     }
 }
