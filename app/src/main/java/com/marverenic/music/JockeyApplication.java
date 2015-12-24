9e7b0b8efb61c58f4016db4307a61a781af06b90
Andrew Bailey <marverenic@gmail.com>
2015-12-23 21:37:35 -0500

    Implemented Glide memory callbacks

--- app/src/main/java/com/marverenic/music/JockeyApplication.java
+++ app/src/main/java/com/marverenic/music/JockeyApplication.java
@@ -1,6 +1,8 @@
 package com.marverenic.music;
 
 import android.app.Application;
+
+import com.bumptech.glide.Glide;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
@@ -12,5 +14,11 @@ public class JockeyApplication extends Application {
         Fabric.with(this, new Crashlytics());
 
         PlayerController.startService(getApplicationContext());
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        Glide.with(this).onTrimMemory(level);
     }
 }
