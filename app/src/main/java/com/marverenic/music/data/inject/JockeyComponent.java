2150ad1d3d82bbbcbf8dbca96d3f8371b6e4575b
Andrew Bailey <marverenic@gmail.com>
2016-05-29 22:39:33 -0400

    Create Dagger skeleton

---
+++ app/src/main/java/com/marverenic/music/data/inject/JockeyComponent.java
@@ -0,0 +1,10 @@
+package com.marverenic.music.data.inject;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component(modules = {ContextModule.class, MediaStoreModule.class})
+public interface JockeyComponent {
+}
