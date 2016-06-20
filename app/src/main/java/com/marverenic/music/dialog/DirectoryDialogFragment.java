bd43643e352900f914c3af28fbbdb521b1b1f7f9
Andrew Bailey <marverenic@gmail.com>
2016-06-19 22:59:59 -0400

    Save current directory on configuration changes

--- app/src/main/java/com/marverenic/music/dialog/DirectoryDialogFragment.java
+++ app/src/main/java/com/marverenic/music/dialog/DirectoryDialogFragment.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
@@ -20,6 +21,8 @@ public class DirectoryDialogFragment extends DialogFragment {
 
     private static final String TAG_DIALOG = "DirectoryDialogFragment_Dialog";
 
+    private static final String KEY_DIRECTORY = "Directory";
+
     private File mDirectory;
     private List<File> mSubDirectories;
     private String[] mSubDirectoryNames;
@@ -33,6 +36,27 @@ public class DirectoryDialogFragment extends DialogFragment {
     public DirectoryDialogFragment setDirectoryPickListener(OnDirectoryPickListener callback) {
         mCallback = callback;
         return this;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            String path = savedInstanceState.getString(KEY_DIRECTORY);
+            if (path != null) {
+                mDirectory = new File(path);
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (mDirectory != null) {
+            outState.putString(KEY_DIRECTORY, mDirectory.getAbsolutePath());
+        }
     }
 
     @NonNull
