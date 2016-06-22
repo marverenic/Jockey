4412623d86d14d1f3305cc029bcfe3a9ec5a4aab
Andrew Bailey <marverenic@gmail.com>
2016-06-21 22:29:26 -0400

    Handle first start dialog using PreferenceStore

--- app/src/main/java/com/marverenic/music/activity/BaseActivity.java
+++ app/src/main/java/com/marverenic/music/activity/BaseActivity.java
@@ -1,7 +1,5 @@
 package com.marverenic.music.activity;
 
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
@@ -19,9 +17,10 @@ import android.widget.TextView;
 
 import com.marverenic.music.BuildConfig;
 import com.marverenic.music.R;
+import com.marverenic.music.data.store.PreferenceStore;
+import com.marverenic.music.data.store.SharedPreferencesStore;
 import com.marverenic.music.player.PlayerController;
 import com.marverenic.music.utils.Navigate;
-import com.marverenic.music.utils.Prefs;
 import com.marverenic.music.utils.Themes;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
@@ -50,40 +49,34 @@ public abstract class BaseActivity extends RxAppCompatActivity
         super.onCreate(savedInstanceState);
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
-        // Show a first start confirmation about privacy
-        // This can't be done in the Application class because it's not allowed to show
-        // AlertDialogs. Additionally, this check has to occur in every Activity since Jockey can
-        // be started from sources other than its main Activity.
+        PreferenceStore preferenceStore = new SharedPreferencesStore(this);
 
-        final SharedPreferences prefs = Prefs.getPrefs(this);
-
-        if (prefs.getBoolean(Prefs.SHOW_FIRST_START, true)) {
-            final View messageView = getLayoutInflater().inflate(R.layout.alert_pref, null);
-            final TextView message = (TextView) messageView.findViewById(R.id.alertMessage);
-            final CheckBox pref = (CheckBox) messageView.findViewById(R.id.alertPref);
-
-            message.setText(Html.fromHtml(getString(R.string.first_launch_detail)));
-            message.setMovementMethod(LinkMovementMethod.getInstance());
-
-            pref.setChecked(true);
-            pref.setText(R.string.enable_additional_logging_detailed);
-
-            new AlertDialog.Builder(this)
-                    .setTitle(R.string.first_launch_title)
-                    .setView(messageView)
-                    .setPositiveButton(R.string.action_agree,
-                            new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            prefs.edit()
-                                    .putBoolean(Prefs.SHOW_FIRST_START, false)
-                                    .putBoolean(Prefs.ALLOW_LOGGING, pref.isChecked())
-                                    .apply();
-                        }
-                    })
-                    .setCancelable(false)
-                    .show();
+        if (preferenceStore.showFirstStart()) {
+            showFirstRunDialog(preferenceStore);
         }
+    }
+
+    private void showFirstRunDialog(PreferenceStore preferenceStore) {
+        View messageView = getLayoutInflater().inflate(R.layout.alert_pref, null);
+        TextView message = (TextView) messageView.findViewById(R.id.alertMessage);
+        CheckBox pref = (CheckBox) messageView.findViewById(R.id.alertPref);
+
+        message.setText(Html.fromHtml(getString(R.string.first_launch_detail)));
+        message.setMovementMethod(LinkMovementMethod.getInstance());
+
+        pref.setChecked(true);
+        pref.setText(R.string.enable_additional_logging_detailed);
+
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.first_launch_title)
+                .setView(messageView)
+                .setPositiveButton(R.string.action_agree,
+                        (dialog, which) -> {
+                            preferenceStore.setAllowLogging(pref.isChecked());
+                            preferenceStore.setShowFirstStart(false);
+                        })
+                .setCancelable(false)
+                .show();
     }
 
     /**
