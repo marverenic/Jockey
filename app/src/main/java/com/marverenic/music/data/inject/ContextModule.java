c115832a2661f210e294886896701bd1b8bb4f12
Andrew Bailey <marverenic@gmail.com>
2016-06-26 15:16:59 -0400

    Provide ThemeStore in ContextModule

--- app/src/main/java/com/marverenic/music/data/inject/ContextModule.java
+++ app/src/main/java/com/marverenic/music/data/inject/ContextModule.java
@@ -3,7 +3,9 @@ package com.marverenic.music.data.inject;
 import android.content.Context;
 
 import com.marverenic.music.data.store.PreferencesStore;
+import com.marverenic.music.data.store.PresetThemeStore;
 import com.marverenic.music.data.store.SharedPreferencesStore;
+import com.marverenic.music.data.store.ThemeStore;
 
 import javax.inject.Singleton;
 
@@ -28,6 +30,12 @@ public class ContextModule {
     @Singleton
     public PreferencesStore providePreferencesStore(Context context) {
         return new SharedPreferencesStore(context);
+    }
+
+    @Provides
+    @Singleton
+    public ThemeStore provideThemeStore(Context context, PreferencesStore preferencesStore) {
+        return new PresetThemeStore(context, preferencesStore);
     }
 
 }
