941822931801aae0a56624727764d4e8b9ecdab5
Andrew Bailey <marverenic@gmail.com>
2016-07-11 23:49:25 -0400

    Implement play count writing

--- app/src/main/java/com/marverenic/music/data/store/LocalPlayCountStore.java
+++ app/src/main/java/com/marverenic/music/data/store/LocalPlayCountStore.java
@@ -2,14 +2,27 @@ package com.marverenic.music.data.store;
 
 import android.content.Context;
 import android.support.v4.util.LongSparseArray;
+import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.marverenic.music.instances.Song;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class LocalPlayCountStore implements PlayCountStore {
+
+    private static final String TAG = "LocalPlayCountStore";
+
+    private static final String PLAY_COUNT_FILENAME = ".playcount";
+    private static final String PLAY_COUNT_HEADER = "This file contains play count information for "
+            + "Jockey and should not be edited";
 
     private Context mContext;
     private final LongSparseArray<Count> mCounts;
@@ -33,9 +46,39 @@ public class LocalPlayCountStore implements PlayCountStore {
                 .subscribeOn(AndroidSchedulers.mainThread());
     }
 
+    private File getPlayCountFile() {
+        return new File(mContext.getExternalFilesDir(null), PLAY_COUNT_FILENAME);
+    }
+
     @Override
     public void save() {
-        // TODO write counts to disk
+        Properties properties = new Properties();
+
+        for (int i = 0; i < mCounts.size(); i++) {
+            String key = Long.toString(mCounts.keyAt(i));
+            String value = mCounts.valueAt(i).getCommaSeparatedValues();
+            properties.put(key, value);
+        }
+
+        try {
+            writePlayCounts(properties);
+        } catch (IOException e) {
+            Log.e(TAG, "save: Failed to write play counts to disk", e);
+            Crashlytics.logException(e);
+        }
+    }
+
+    private void writePlayCounts(Properties playCounts) throws IOException {
+        FileWriter fileWriter = null;
+
+        try {
+            fileWriter = new FileWriter(getPlayCountFile());
+            playCounts.store(fileWriter, PLAY_COUNT_HEADER);
+        } finally {
+            if (fileWriter != null) {
+                fileWriter.close();
+            }
+        }
     }
 
     @Override
