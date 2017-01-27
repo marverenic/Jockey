52c58a86ef67deba083833f8918107ce39b1914b
Andrew Bailey <marverenic@gmail.com>
2017-01-27 15:18:06 -0500

    Create compact widget

---
+++ app/src/main/java/com/marverenic/music/widget/CompactWidget.java
@@ -0,0 +1,82 @@
+package com.marverenic.music.widget;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.widget.RemoteViews;
+
+import com.marverenic.music.R;
+import com.marverenic.music.activity.NowPlayingActivity;
+import com.marverenic.music.model.Song;
+import com.marverenic.music.utils.MediaStyleHelper;
+
+import rx.Observable;
+import timber.log.Timber;
+
+import static android.view.KeyEvent.KEYCODE_MEDIA_NEXT;
+import static android.view.KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE;
+import static android.view.KeyEvent.KEYCODE_MEDIA_PREVIOUS;
+
+public class CompactWidget extends BaseWidget {
+
+    @Override
+    protected void onUpdate(Context context) {
+        Observable.just(createBaseView(context))
+                .flatMap(views -> mPlayerController.getNowPlaying().take(1)
+                        .map(song -> setSong(context, views, song)))
+                .flatMap(views -> mPlayerController.isPlaying().take(1)
+                        .map(isPlaying -> setPlaying(views, isPlaying)))
+                .subscribe(views -> updateAllInstances(context, views),
+                        throwable -> Timber.e(throwable, "Failed to update widget"));
+    }
+
+    private RemoteViews createBaseView(Context context) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_compact);
+
+        Intent launcherIntent = NowPlayingActivity.newIntent(context);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launcherIntent, 0);
+        views.setOnClickPendingIntent(R.id.widget_compact_container, pendingIntent);
+
+        views.setOnClickPendingIntent(R.id.widget_next,
+                MediaStyleHelper.getActionIntent(context, KEYCODE_MEDIA_NEXT));
+
+        views.setOnClickPendingIntent(R.id.widget_play_pause,
+                MediaStyleHelper.getActionIntent(context, KEYCODE_MEDIA_PLAY_PAUSE));
+
+        views.setOnClickPendingIntent(R.id.widget_previous,
+                MediaStyleHelper.getActionIntent(context, KEYCODE_MEDIA_PREVIOUS));
+
+        views.setInt(R.id.widget_next, "setColorFilter", Color.BLACK);
+        views.setInt(R.id.widget_play_pause, "setColorFilter", Color.BLACK);
+        views.setInt(R.id.widget_previous, "setColorFilter", Color.BLACK);
+
+        return views;
+    }
+
+    private static RemoteViews setSong(Context context, RemoteViews views, @Nullable Song song) {
+        if (song == null) {
+            String defaultSongName = context.getString(R.string.nothing_playing);
+
+            views.setTextViewText(R.id.widget_compact_title, defaultSongName);
+            views.setTextViewText(R.id.widget_compact_detail, "");
+        } else {
+            views.setTextViewText(R.id.widget_compact_title, song.getSongName());
+            views.setTextViewText(R.id.widget_compact_detail,
+                    song.getArtistName() + " – " + song.getAlbumName());
+        }
+
+        return views;
+    }
+
+    private static RemoteViews setPlaying(RemoteViews views, boolean isPlaying) {
+        views.setImageViewResource(R.id.widget_play_pause,
+                (isPlaying)
+                        ? R.drawable.ic_pause_36dp
+                        : R.drawable.ic_play_arrow_36dp);
+
+        return views;
+    }
+
+}
