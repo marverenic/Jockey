3cb4d304b96aca80300a073da2858db6f1996f64
Andrew Bailey <marverenic@gmail.com>
2017-02-01 17:00:55 -0500

    Manually generate color preview drawable
    This fixes an issue where the color preview drawable would not be tinted correctly on pre-Nougat versions of Android

--- app/src/main/java/android/support/v7/preference/ColorPreference.java
+++ app/src/main/java/android/support/v7/preference/ColorPreference.java
@@ -2,20 +2,23 @@ package android.support.v7.preference;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.ColorInt;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.widget.ImageView;
 
 import com.marverenic.music.R;
 
 public class ColorPreference extends IntListPreference {
 
+    private static final int STROKE_THICKNESS_DP = 2;
     private static final int[] ATTRIBUTES = {R.attr.colorEntries};
 
     private ImageView mColorPreview;
-    private Drawable mColorDrawable;
+    private GradientDrawable mColorDrawable;
+    private int mStrokeThickness;
     private int[] mColors;
 
     public ColorPreference(Context context, AttributeSet attrs) {
@@ -28,19 +31,30 @@ public class ColorPreference extends IntListPreference {
         mColors = context.getResources().getIntArray(arrayRes);
 
         a.recycle();
+
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        mStrokeThickness = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                STROKE_THICKNESS_DP, metrics);
     }
 
     @Override
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
         mColorPreview = (ImageView) holder.findViewById(R.id.pref_color_preview);
-        mColorDrawable = mColorPreview.getDrawable();
+        mColorDrawable = new GradientDrawable();
+        mColorDrawable.setShape(GradientDrawable.OVAL);
 
         setColor(mColors[findIndexOfValue(getValue())]);
     }
 
     private void setColor(@ColorInt int color) {
-        mColorDrawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        int rgb = 0xFFFFFF & color; // discard alpha bits
+        int centerColor = (0xFF << 24) | rgb; // 100% opaque
+        int strokeColor = (0x80 << 24) | rgb; // 50% transparent
+
+        mColorDrawable.setColor(centerColor);
+        mColorDrawable.setStroke(mStrokeThickness, strokeColor);
+
         mColorPreview.setImageDrawable(mColorDrawable);
     }
 }
