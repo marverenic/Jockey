23335b49c3914b66b9d3bbe32dd9c3c5212a9e06
Andrew Bailey <marverenic@gmail.com>
2017-02-14 16:59:58 -0500

    Enable fast scrolling on the playlist page

--- app/src/main/java/com/marverenic/music/adapter/HeterogeneousFastScrollAdapter.java
+++ app/src/main/java/com/marverenic/music/adapter/HeterogeneousFastScrollAdapter.java
@@ -9,17 +9,10 @@ import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView.S
 public class HeterogeneousFastScrollAdapter extends HeterogeneousAdapter
         implements SectionedAdapter {
 
-    private Coordinate mSectionCoordinate = new Coordinate();
+    private static final int LAST_SECTION_INDEX = -1;
+    private static final int FIRST_SECTION_INDEX = -2;
 
-    @Override
-    public HeterogeneousAdapter addSection(@NonNull Section section, int index) {
-        if (section instanceof SectionedAdapter) {
-            return super.addSection(section, index);
-        } else {
-            throw new IllegalArgumentException("Cannot add a Section that does not implement the " +
-                    "SectionedAdapter interface");
-        }
-    }
+    private Coordinate mSectionCoordinate = new Coordinate();
 
     @NonNull
     @Override
@@ -27,12 +20,38 @@ public class HeterogeneousFastScrollAdapter extends HeterogeneousAdapter
         lookupCoordinates(position, mSectionCoordinate);
         Section<?> section = getSection(mSectionCoordinate.getSection());
 
+        /*
+            If the section at the position doesn't implement SectionedAdapter, linearly walk up
+            and down sections to find the nearest section that does implement the interface, and
+            use the adjacent section name instead
+         */
+        int walk = -1;
+        while (!(section instanceof SectionedAdapter) && mSectionCoordinate.getSection() + walk >= 0
+                && mSectionCoordinate.getSection() + walk < getSectionCount()) {
+
+            section = getSection(mSectionCoordinate.getSection() + walk);
+            if (walk > 0) {
+                mSectionCoordinate.setItemIndex(FIRST_SECTION_INDEX);
+                walk = -(walk + 1);
+            } else {
+                mSectionCoordinate.setItemIndex(LAST_SECTION_INDEX);
+                walk = -(walk - 1);
+            }
+        }
+
         if (section instanceof SectionedAdapter) {
             SectionedAdapter sectionedAdapter = (SectionedAdapter) section;
-            return sectionedAdapter.getSectionName(mSectionCoordinate.getItemIndex());
+            int index = mSectionCoordinate.getItemIndex();
+
+            if (index == FIRST_SECTION_INDEX) {
+                index = 0;
+            } else if (index == LAST_SECTION_INDEX) {
+                index = section.getItemCount(this) - 1;
+            }
+
+            return sectionedAdapter.getSectionName(index);
         } else {
-            throw new IllegalStateException("One of the attached Sections does not implement " +
-                    "the SectionedAdapter interface (index = " + position);
+            return "";
         }
     }
 }
--- app/src/main/java/com/marverenic/music/adapter/PlaylistSection.java
+++ app/src/main/java/com/marverenic/music/adapter/PlaylistSection.java
@@ -5,14 +5,17 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import com.marverenic.music.databinding.InstancePlaylistBinding;
+import com.marverenic.music.model.ModelUtil;
 import com.marverenic.music.model.Playlist;
 import com.marverenic.heterogeneousadapter.EnhancedViewHolder;
 import com.marverenic.heterogeneousadapter.HeterogeneousAdapter;
 import com.marverenic.music.viewmodel.PlaylistViewModel;
+import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView;
 
 import java.util.List;
 
-public class PlaylistSection extends HeterogeneousAdapter.ListSection<Playlist> {
+public class PlaylistSection extends HeterogeneousAdapter.ListSection<Playlist>
+        implements FastScrollRecyclerView.SectionedAdapter {
 
     public PlaylistSection(@NonNull List<Playlist> data) {
         super(data);
@@ -27,6 +30,13 @@ public class PlaylistSection extends HeterogeneousAdapter.ListSection<Playlist> 
     @Override
     public int getId(int position) {
         return (int) get(position).getPlaylistId();
+    }
+
+    @NonNull
+    @Override
+    public String getSectionName(int position) {
+        char firstChar = ModelUtil.sortableTitle(get(position).getPlaylistName()).charAt(0);
+        return Character.toString(firstChar).toUpperCase();
     }
 
     public static class ViewHolder extends EnhancedViewHolder<Playlist> {
