1555fb012742ee76c1db2cd6ddfbb0c4d01251c2
Andrew Bailey <marverenic@gmail.com>
2017-07-05 22:31:53 -0400

    Move library view logic into fragment

--- app/src/main/java/com/marverenic/music/ui/library/LibraryActivity.java
+++ app/src/main/java/com/marverenic/music/ui/library/LibraryActivity.java
@@ -1,35 +1,18 @@
 package com.marverenic.music.ui.library;
 
-import android.Manifest;
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 import com.marverenic.music.JockeyApplication;
 import com.marverenic.music.R;
 import com.marverenic.music.data.store.MediaStoreUtil;
-import com.marverenic.music.data.store.MusicStore;
-import com.marverenic.music.data.store.PlaylistStore;
-import com.marverenic.music.data.store.PreferenceStore;
-import com.marverenic.music.data.store.ThemeStore;
 import com.marverenic.music.model.Song;
 import com.marverenic.music.player.PlayerController;
 import com.marverenic.music.ui.BaseLibraryActivity;
-import com.marverenic.music.ui.about.AboutActivity;
-import com.marverenic.music.ui.common.playlist.CreatePlaylistDialogFragment;
-import com.marverenic.music.ui.library.playlist.edit.AutoPlaylistEditActivity;
-import com.marverenic.music.ui.search.SearchActivity;
-import com.marverenic.music.ui.settings.SettingsActivity;
 import com.marverenic.music.utils.UriUtils;
-import com.marverenic.music.utils.Util;
 
 import java.io.File;
 import java.util.Collections;
@@ -37,21 +20,13 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import rx.Observable;
 import timber.log.Timber;
 
-public class LibraryActivity extends BaseLibraryActivity implements View.OnClickListener {
+public class LibraryActivity extends BaseLibraryActivity {
 
-    private static final String TAG_MAKE_PLAYLIST = "CreatePlaylistDialog";
     private static final String ACTION_SHOW_NOW_PLAYING_PAGE = "LibraryActivity.ShowNowPlayingPage";
 
-    @Inject ThemeStore mThemeStore;
-    @Inject MusicStore mMusicStore;
     @Inject PlayerController mPlayerController;
-    @Inject PlaylistStore mPlaylistStore;
-    @Inject PreferenceStore mPrefStore;
-
-    private SwipeRefreshLayout mRefreshLayout;
 
     public static Intent newNowPlayingIntent(Context context) {
         Intent intent = new Intent(context, LibraryActivity.class);
@@ -70,44 +45,6 @@ public class LibraryActivity extends BaseLibraryActivity implements View.OnClick
 
         JockeyApplication.getComponent(this).inject(this);
         onNewIntent(getIntent());
-
-        /*
-        initRefreshLayout();
-        mMusicStore.loadAll();
-        mPlaylistStore.loadPlaylists();
-
-        // Setup the FAB
-        FABMenu fab = (FABMenu) findViewById(R.id.fab);
-        fab.addChild(R.drawable.ic_add_24dp, this, R.string.playlist);
-        fab.addChild(R.drawable.ic_add_24dp, this, R.string.playlist_auto);
-
-        ViewPager pager = (ViewPager) findViewById(R.id.library_pager);
-
-        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.library_app_bar_layout);
-        appBarLayout.addOnOffsetChangedListener((layout, verticalOffset) ->
-                pager.setPadding(pager.getPaddingLeft(),
-                        pager.getPaddingTop(),
-                        pager.getPaddingRight(),
-                        layout.getTotalScrollRange() + verticalOffset));
-
-        int page = mPrefStore.getDefaultPage();
-        if (page != 0 || !hasRwPermission()) {
-            fab.setVisibility(View.GONE);
-        }
-
-        LibraryPagerAdapter adapter = new LibraryPagerAdapter(this, getSupportFragmentManager());
-        adapter.setFloatingActionButton(fab);
-        pager.setAdapter(adapter);
-        pager.addOnPageChangeListener(adapter);
-        ((TabLayout) findViewById(R.id.library_tabs)).setupWithViewPager(pager);
-
-        pager.setCurrentItem(page);
-
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-            getSupportActionBar().setHomeButtonEnabled(false);
-            getSupportActionBar().setDisplayShowHomeEnabled(false);
-        }*/
     }
 
     @Override
@@ -199,71 +136,6 @@ public class LibraryActivity extends BaseLibraryActivity implements View.OnClick
     private void startIntentQueue(List<Song> queue, int position) {
         mPlayerController.setQueue(queue, position);
         mPlayerController.play();
-    }
-
-    private void initRefreshLayout() {
-        mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.library_refresh_layout);
-        mRefreshLayout.setSize(SwipeRefreshLayout.DEFAULT);
-        mRefreshLayout.setColorSchemeColors(mThemeStore.getPrimaryColor(),
-                mThemeStore.getAccentColor());
-        mRefreshLayout.setEnabled(false);
-
-        Observable.combineLatest(mMusicStore.isLoading(), mPlaylistStore.isLoading(),
-                (musicLoading, playlistLoading) -> {
-                    return musicLoading || playlistLoading;
-                })
-                .compose(bindToLifecycle())
-                .subscribe(
-                        refreshing -> {
-                            mRefreshLayout.setEnabled(refreshing);
-                            mRefreshLayout.setRefreshing(refreshing);
-                        }, throwable -> {
-                            Timber.e(throwable, "Failed to update refresh indicator");
-                        });
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    private boolean hasRwPermission() {
-        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Util.hasPermissions(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_library, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_library_settings:
-                startActivity(SettingsActivity.newIntent(this));
-                return true;
-            case R.id.menu_library_search:
-                startActivity(SearchActivity.newIntent(this));
-                return true;
-            case R.id.menu_library_about:
-                startActivity(AboutActivity.newIntent(this));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getTag() != null) {
-            if (v.getTag().equals("fab-" + getString(R.string.playlist))) {
-                new CreatePlaylistDialogFragment.Builder(getSupportFragmentManager())
-                        .showSnackbarIn(R.id.library_pager)
-                        .show(TAG_MAKE_PLAYLIST);
-            } else if (v.getTag().equals("fab-" + getString(R.string.playlist_auto))) {
-                startActivity(AutoPlaylistEditActivity.newIntent(this));
-            }
-        }
     }
 
     @Override
--- app/src/main/java/com/marverenic/music/ui/library/LibraryFragment.java
+++ app/src/main/java/com/marverenic/music/ui/library/LibraryFragment.java
@@ -2,15 +2,47 @@ package com.marverenic.music.ui.library;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.marverenic.music.JockeyApplication;
 import com.marverenic.music.R;
+import com.marverenic.music.data.store.MediaStoreUtil;
+import com.marverenic.music.data.store.MusicStore;
+import com.marverenic.music.data.store.PlaylistStore;
+import com.marverenic.music.data.store.PreferenceStore;
+import com.marverenic.music.data.store.ThemeStore;
 import com.marverenic.music.ui.BaseFragment;
+import com.marverenic.music.ui.about.AboutActivity;
+import com.marverenic.music.ui.common.playlist.CreatePlaylistDialogFragment;
+import com.marverenic.music.ui.library.playlist.edit.AutoPlaylistEditActivity;
+import com.marverenic.music.ui.search.SearchActivity;
+import com.marverenic.music.ui.settings.SettingsActivity;
+import com.marverenic.music.view.FABMenu;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+import timber.log.Timber;
 
 public class LibraryFragment extends BaseFragment {
+
+    private static final String TAG_MAKE_PLAYLIST = "CreatePlaylistDialog";
+
+    @Inject MusicStore mMusicStore;
+    @Inject PlaylistStore mPlaylistStore;
+    @Inject ThemeStore mThemeStore;
+    @Inject PreferenceStore mPrefStore;
+
+    private SwipeRefreshLayout mRefreshLayout;
 
     public static LibraryFragment newInstance() {
         return new LibraryFragment();
@@ -27,6 +59,91 @@ public class LibraryFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.activity_library, container, false);
+        View root = inflater.inflate(R.layout.activity_library, container, false);
+
+        initRefreshLayout((SwipeRefreshLayout) root.findViewById(R.id.library_refresh_layout));
+        mMusicStore.loadAll();
+        mPlaylistStore.loadPlaylists();
+
+        // Setup the FAB
+        FABMenu fab = (FABMenu) root.findViewById(R.id.fab);
+        fab.addChild(R.drawable.ic_add_24dp, v -> {
+            new CreatePlaylistDialogFragment.Builder(getFragmentManager())
+                    .showSnackbarIn(R.id.library_pager)
+                    .show(TAG_MAKE_PLAYLIST);
+        }, R.string.playlist);
+
+        fab.addChild(R.drawable.ic_add_24dp, v -> {
+            startActivity(AutoPlaylistEditActivity.newIntent(getContext()));
+        }, R.string.playlist_auto);
+
+        ViewPager pager = (ViewPager) root.findViewById(R.id.library_pager);
+
+        AppBarLayout appBarLayout = (AppBarLayout) root.findViewById(R.id.library_app_bar_layout);
+        appBarLayout.addOnOffsetChangedListener((layout, verticalOffset) ->
+                pager.setPadding(pager.getPaddingLeft(),
+                        pager.getPaddingTop(),
+                        pager.getPaddingRight(),
+                        layout.getTotalScrollRange() + verticalOffset));
+
+        int page = mPrefStore.getDefaultPage();
+        if (page != 0 || !MediaStoreUtil.hasPermission(getContext())) {
+            fab.setVisibility(View.GONE);
+        }
+
+        LibraryPagerAdapter adapter = new LibraryPagerAdapter(getContext(), getFragmentManager());
+        adapter.setFloatingActionButton(fab);
+        pager.setAdapter(adapter);
+        pager.addOnPageChangeListener(adapter);
+        ((TabLayout) root.findViewById(R.id.library_tabs)).setupWithViewPager(pager);
+
+        setHasOptionsMenu(true);
+
+        pager.setCurrentItem(page);
+        return root;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.activity_library, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_library_settings:
+                startActivity(SettingsActivity.newIntent(getContext()));
+                return true;
+            case R.id.menu_library_search:
+                startActivity(SearchActivity.newIntent(getContext()));
+                return true;
+            case R.id.menu_library_about:
+                startActivity(AboutActivity.newIntent(getContext()));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void initRefreshLayout(SwipeRefreshLayout swipeRefreshLayout) {
+        mRefreshLayout = swipeRefreshLayout;
+        mRefreshLayout.setSize(SwipeRefreshLayout.DEFAULT);
+        mRefreshLayout.setColorSchemeColors(mThemeStore.getPrimaryColor(),
+                mThemeStore.getAccentColor());
+        mRefreshLayout.setEnabled(false);
+
+        Observable.combineLatest(mMusicStore.isLoading(), mPlaylistStore.isLoading(),
+                (musicLoading, playlistLoading) -> {
+                    return musicLoading || playlistLoading;
+                })
+                .compose(bindToLifecycle())
+                .subscribe(
+                        refreshing -> {
+                            mRefreshLayout.setEnabled(refreshing);
+                            mRefreshLayout.setRefreshing(refreshing);
+                        }, throwable -> {
+                            Timber.e(throwable, "Failed to update refresh indicator");
+                        });
     }
 }
