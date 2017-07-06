a8a9ba4c5c242ba440b12b9f4d2366434bbe3549
Andrew Bailey <marverenic@gmail.com>
2017-07-05 23:15:55 -0400

    Setup Databinding for library fragment

--- app/src/main/java/com/marverenic/music/ui/library/LibraryFragment.java
+++ app/src/main/java/com/marverenic/music/ui/library/LibraryFragment.java
@@ -3,7 +3,6 @@ package com.marverenic.music.ui.library;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
@@ -20,13 +19,13 @@ import com.marverenic.music.data.store.MusicStore;
 import com.marverenic.music.data.store.PlaylistStore;
 import com.marverenic.music.data.store.PreferenceStore;
 import com.marverenic.music.data.store.ThemeStore;
+import com.marverenic.music.databinding.FragmentLibraryBinding;
 import com.marverenic.music.ui.BaseFragment;
 import com.marverenic.music.ui.about.AboutActivity;
 import com.marverenic.music.ui.common.playlist.CreatePlaylistDialogFragment;
 import com.marverenic.music.ui.library.playlist.edit.AutoPlaylistEditActivity;
 import com.marverenic.music.ui.search.SearchActivity;
 import com.marverenic.music.ui.settings.SettingsActivity;
-import com.marverenic.music.view.FABMenu;
 
 import javax.inject.Inject;
 
@@ -42,7 +41,7 @@ public class LibraryFragment extends BaseFragment {
     @Inject ThemeStore mThemeStore;
     @Inject PreferenceStore mPrefStore;
 
-    private SwipeRefreshLayout mRefreshLayout;
+    private FragmentLibraryBinding mBinding;
 
     public static LibraryFragment newInstance() {
         return new LibraryFragment();
@@ -59,27 +58,26 @@ public class LibraryFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.activity_library, container, false);
+        mBinding = FragmentLibraryBinding.inflate(inflater, container, false);
 
-        initRefreshLayout((SwipeRefreshLayout) root.findViewById(R.id.library_refresh_layout));
+        initRefreshLayout();
         mMusicStore.loadAll();
         mPlaylistStore.loadPlaylists();
 
         // Setup the FAB
-        FABMenu fab = (FABMenu) root.findViewById(R.id.fab);
-        fab.addChild(R.drawable.ic_add_24dp, v -> {
+        mBinding.fab.addChild(R.drawable.ic_add_24dp, v -> {
             new CreatePlaylistDialogFragment.Builder(getFragmentManager())
                     .showSnackbarIn(R.id.library_pager)
                     .show(TAG_MAKE_PLAYLIST);
         }, R.string.playlist);
 
-        fab.addChild(R.drawable.ic_add_24dp, v -> {
+        mBinding.fab.addChild(R.drawable.ic_add_24dp, v -> {
             startActivity(AutoPlaylistEditActivity.newIntent(getContext()));
         }, R.string.playlist_auto);
 
-        ViewPager pager = (ViewPager) root.findViewById(R.id.library_pager);
+        ViewPager pager = mBinding.libraryPager;
+        AppBarLayout appBarLayout = mBinding.libraryAppBarLayout;
 
-        AppBarLayout appBarLayout = (AppBarLayout) root.findViewById(R.id.library_app_bar_layout);
         appBarLayout.addOnOffsetChangedListener((layout, verticalOffset) ->
                 pager.setPadding(pager.getPaddingLeft(),
                         pager.getPaddingTop(),
@@ -88,19 +86,19 @@ public class LibraryFragment extends BaseFragment {
 
         int page = mPrefStore.getDefaultPage();
         if (page != 0 || !MediaStoreUtil.hasPermission(getContext())) {
-            fab.setVisibility(View.GONE);
+            mBinding.fab.setVisibility(View.GONE);
         }
 
         LibraryPagerAdapter adapter = new LibraryPagerAdapter(getContext(), getFragmentManager());
-        adapter.setFloatingActionButton(fab);
+        adapter.setFloatingActionButton(mBinding.fab);
         pager.setAdapter(adapter);
         pager.addOnPageChangeListener(adapter);
-        ((TabLayout) root.findViewById(R.id.library_tabs)).setupWithViewPager(pager);
+        mBinding.libraryTabs.setupWithViewPager(pager);
 
         setHasOptionsMenu(true);
 
         pager.setCurrentItem(page);
-        return root;
+        return mBinding.getRoot();
     }
 
     @Override
@@ -125,13 +123,12 @@ public class LibraryFragment extends BaseFragment {
         }
     }
 
-
-    private void initRefreshLayout(SwipeRefreshLayout swipeRefreshLayout) {
-        mRefreshLayout = swipeRefreshLayout;
-        mRefreshLayout.setSize(SwipeRefreshLayout.DEFAULT);
-        mRefreshLayout.setColorSchemeColors(mThemeStore.getPrimaryColor(),
+    private void initRefreshLayout() {
+        SwipeRefreshLayout swipeRefreshLayout = mBinding.libraryRefreshLayout;
+        swipeRefreshLayout.setSize(SwipeRefreshLayout.DEFAULT);
+        swipeRefreshLayout.setColorSchemeColors(mThemeStore.getPrimaryColor(),
                 mThemeStore.getAccentColor());
-        mRefreshLayout.setEnabled(false);
+        swipeRefreshLayout.setEnabled(false);
 
         Observable.combineLatest(mMusicStore.isLoading(), mPlaylistStore.isLoading(),
                 (musicLoading, playlistLoading) -> {
@@ -140,8 +137,8 @@ public class LibraryFragment extends BaseFragment {
                 .compose(bindToLifecycle())
                 .subscribe(
                         refreshing -> {
-                            mRefreshLayout.setEnabled(refreshing);
-                            mRefreshLayout.setRefreshing(refreshing);
+                            swipeRefreshLayout.setEnabled(refreshing);
+                            swipeRefreshLayout.setRefreshing(refreshing);
                         }, throwable -> {
                             Timber.e(throwable, "Failed to update refresh indicator");
                         });
---
+++ app/src/main/java/com/marverenic/music/ui/library/LibraryViewModel.java
@@ -0,0 +1,9 @@
+package com.marverenic.music.ui.library;
+
+import android.databinding.BaseObservable;
+
+public class LibraryViewModel extends BaseObservable {
+
+
+
+}
