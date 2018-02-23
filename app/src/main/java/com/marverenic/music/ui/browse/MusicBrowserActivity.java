186f34325dd94a006796c6f783fd1ff769d59ef1
Andrew Bailey <marverenic@gmail.com>
2018-02-23 12:53:00 -0500

    Show error if a song's folder couldn't be determined

--- app/src/main/java/com/marverenic/music/ui/browse/MusicBrowserActivity.java
+++ app/src/main/java/com/marverenic/music/ui/browse/MusicBrowserActivity.java
@@ -7,8 +7,14 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.marverenic.music.R;
 import com.marverenic.music.model.Song;
+import com.marverenic.music.ui.BaseToolbarFragment;
 import com.marverenic.music.ui.SingleFragmentActivity;
 import com.marverenic.music.utils.UriUtils;
 
@@ -20,7 +26,13 @@ public class MusicBrowserActivity extends SingleFragmentActivity {
 
     public static Intent newIntent(Context context, Song targetSong) {
         String path = UriUtils.getPathFromUri(context, targetSong.getLocation());
-        return newIntent(context, new File(path).getParentFile());
+        if (path != null) {
+            path = new File(path).getParent();
+        }
+
+        Intent intent = new Intent(context, MusicBrowserActivity.class);
+        intent.putExtra(EXTRA_STARTING_DIRECTORY, path);
+        return intent;
     }
 
     public static Intent newIntent(Context context, File startingDirectory) {
@@ -32,7 +44,11 @@ public class MusicBrowserActivity extends SingleFragmentActivity {
     @Override
     protected Fragment onCreateFragment(Bundle savedInstanceState) {
         String startingPath = getIntent().getStringExtra(EXTRA_STARTING_DIRECTORY);
-        return MusicBrowserFragment.newInstance(new File(startingPath), false);
+        if (startingPath == null) {
+            return UnknownLocationFragment.newInstance();
+        } else {
+            return MusicBrowserFragment.newInstance(new File(startingPath), false);
+        }
     }
 
     @Override
@@ -41,6 +57,32 @@ public class MusicBrowserActivity extends SingleFragmentActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class UnknownLocationFragment extends BaseToolbarFragment {
+
+        public static UnknownLocationFragment newInstance() {
+            return new UnknownLocationFragment();
+        }
+
+        @Override
+        protected View onCreateContentView(LayoutInflater inflater, @Nullable ViewGroup container,
+                                           @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.instance_empty, container, false);
+            TextView title = view.findViewById(R.id.empty_message);
+            title.setText(R.string.empty_error_folder);
+            return view;
+        }
+
+        @Override
+        protected String getFragmentTitle() {
+            return getString(R.string.header_browser);
+        }
+
+        @Override
+        protected boolean canNavigateUp() {
+            return true;
         }
     }
 
