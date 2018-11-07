8848b7a44dc6d4bcd2e65772194382dffcb36d47
Andrew Bailey <marverenic@gmail.com>
2018-11-06 20:15:30 -0500

    Add additional options for default page setting

--- app/src/main/java/com/marverenic/music/data/annotations/StartPage.java
+++ app/src/main/java/com/marverenic/music/data/annotations/StartPage.java
@@ -3,11 +3,13 @@ package com.marverenic.music.data.annotations;
 import android.support.annotation.IntDef;
 
 @IntDef(value = {StartPage.PLAYLISTS, StartPage.SONGS, StartPage.ARTISTS, StartPage.ALBUMS,
-        StartPage.GENRES})
+        StartPage.GENRES, StartPage.BROWSER, StartPage.RECENTLY_ADDED})
 public @interface StartPage {
     int PLAYLISTS = 0;
     int SONGS = 1;
     int ARTISTS = 2;
     int ALBUMS = 3;
     int GENRES = 4;
+    int BROWSER = 5;
+    int RECENTLY_ADDED = 6;
 }
